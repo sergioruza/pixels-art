447e900ecaa597af8c97268d43a72e49e9bfc537
Sergio <sergioruza22@gmail.com>
2022-07-04 19:08:44 -0300

    Requisito quatro e cinco, ok

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
+/* eslint-disable no-extra-semi */
 const idPaleta = document.querySelector('#color-palette');
 const buttons = document.querySelectorAll('button');
-console.log(buttons);
+
 
 let black = buttons[0].style.backgroundColor = 'black';
 let blue = buttons[1].style.backgroundColor = 'blue';
 let green = buttons[2].style.backgroundColor = 'green';
 let purple = buttons[3].style.backgroundColor = 'purple';
+
+let pixelBoard = document.querySelector('#pixel-board');
+function criaQuadro() {
+  for (let i = 1; i <= 25; i += 1) {
+    let criaDiv = document.createElement('div');
+    pixelBoard.appendChild(criaDiv);
+    criaDiv.className = 'pixel';
+  }
+}
+criaQuadro();
+
+let pixels = document.querySelectorAll('.pixel');
+function quebraLinha() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    if (i === 5 || i === 10 || i === 15 || i === 20) {
+      let criaBr = document.createElement('br');
+      pixelBoard.insertBefore(criaBr, pixels[i]);
+    }
+  }
+};
+quebraLinha();
+
+// let corSelecionada = ''
+// buttons[0].addEventListener('click', function () {
+//  corSelecionada =
+// })
