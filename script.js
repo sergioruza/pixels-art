62e2dd55ccbb54971f5d5da9b5544265ea7873ce
Sergio <sergioruza22@gmail.com>
2022-07-04 15:38:27 -0300

    Requisito dois e três, ok

---
+++ script.js
@@ -0,0 +1,8 @@
+const idPaleta = document.querySelector('#color-palette');
+const buttons = document.querySelectorAll('button');
+console.log(buttons);
+
+let black = buttons[0].style.backgroundColor = 'black';
+let blue = buttons[1].style.backgroundColor = 'blue';
+let green = buttons[2].style.backgroundColor = 'green';
+let purple = buttons[3].style.backgroundColor = 'purple';
